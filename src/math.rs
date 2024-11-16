32cfa79cebd7a5f761dcfe4de7cf823b78b420d1
Ivan Sysoev <vanysysoev@mail.ru>
2024-11-16 16:05:57 +0300

    fixing math

--- src/math.rs
+++ src/math.rs
@@ -1,31 +1,57 @@
 
-pub fn calcSpotPrice(tokenBalanceIn: U256, tokenWeightIn: U256, tokenBalanceOut: U256, 
-    tokenWeightOut: U256, swapFee: U256) -> Result<(), Error> {
-    let numer = tokenBalanceIn / tokenWeightIn;
-    let denom = tokenBalanceOut / tokenWeightOut;
-    let ratio = numer / denom;
-    let scale = 1 / (1 - swapFee);
-    let spotPrice = ratio * scale ;
-    return spotPrice 
-    }
+pub fn calc_spot_price(
+    token_balance_in: U256,
+    token_weight_in: U256,
+    token_balance_out: U256,
+    token_weight_out: U256,
+    swap_fee: U256,
+) -> Result<U256, Error> {
+    let numer = token_balance_in.checked_div(token_weight_in).ok_or(Error::Math)?;
+    let denom = token_balance_out.checked_div(token_weight_out).ok_or(Error::Math)?;
+    let ratio = numer.checked_div(denom).ok_or(Error::Math)?;
+    let scale = U256::from(1).checked_div(U256::from(1) - swap_fee).ok_or(Error::Math)?;
+    let spot_price = ratio.checked_mul(scale).ok_or(Error::Math)?;
+    Ok(spot_price)
+}
 
-pub fn calcOutGivenIn(tokenBalanceIn: U256, tokenWeightIn: U256, tokenBalanceOut: U256, 
-    tokenWeightOut: U256, tokenAmountIn: U256, swapFee: U256) -> Result<(), Error> { 
-        let weightRatio = tokenWeightIn / tokenWeightOut;
-        let adjustedIn = (1 - swapFee) * tokenAmountIn;
-        let y  =  (tokenBalanceIn / (tokenBalanceIn + adjustedIn));
-        let foo =  y.powf(weightRatio);
-        let bar = 1 - foo;
-        tokenBalanceOut*bar; 
-    }
-pub fn calcInGivenOut(tokenBalanceIn: U256, tokenWeightIn: U256, tokenBalanceOut: U256, 
-    tokenWeightOut: U256, tokenAmountOut: U256, swapFee: U256) -> Result<(), Error> {
-        let weightRatio = tokenWeightOut / tokenWeightIn;
-        let y = tokenBalanceOut / (tokenBalanceOut - tokenAmountOut);
-        let foo = y.powf(weightRatio)-1;
-        tokenBalanceIn * (foo) / (1-swapFee)
+pub fn calc_out_given_in(
+    token_balance_in: U256,
+    token_weight_in: U256,
+    token_balance_out: U256,
+    token_weight_out: U256,
+    token_amount_in: U256,
+    swap_fee: U256,
+) -> Result<U256, Error> {
+    let weight_ratio = token_weight_in.checked_div(token_weight_out).ok_or(Error::Math)?;
+    let adjusted_in = (U256::from(1) - swap_fee)
+        .checked_mul(token_amount_in)
+        .ok_or(Error::Math)?;
+    let y = token_balance_in
+        .checked_div(token_balance_in + adjusted_in)
+        .ok_or(Error::Math)?;
+    let foo = y.checked_pow(weight_ratio).ok_or(Error::Math)?;
+    let bar = U256::from(1) - foo;
+    let result = token_balance_out.checked_mul(bar).ok_or(Error::Math)?;
+    Ok(result)
+}
 
-    }
-
-println!("{}",calcSpotPrice(100, 100, 100,500));
+pub fn calc_in_given_out(
+    token_balance_in: U256,
+    token_weight_in: U256,
+    token_balance_out: U256,
+    token_weight_out: U256,
+    token_amount_out: U256,
+    swap_fee: U256,
+) -> Result<U256, Error> {
+    let weight_ratio = token_weight_out.checked_div(token_weight_in).ok_or(Error::Math)?;
+    let y = token_balance_out
+        .checked_div(token_balance_out - token_amount_out)
+        .ok_or(Error::Math)?;
+    let foo = y.checked_pow(weight_ratio).ok_or(Error::Math)?;
+    let fee_adjusted = (foo - U256::from(1))
+        .checked_div(U256::from(1) - swap_fee)
+        .ok_or(Error::Math)?;
+    let result = token_balance_in.checked_mul(fee_adjusted).ok_or(Error::Math)?;
+    Ok(result)
+}
 
